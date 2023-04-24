c8e605418b134042a3aa5fbafa6344638f249e9c
Ndimande <mkatroso.deep@gmail.com>
2023-04-24 21:47:23 +0200

    IWA9

--- Scripts.js
+++ Scripts.js
@@ -1,37 +1,38 @@
 const salary = 4000;
-const lodging = 'apartment'
-const size = 'large'
-
+const lodging = 'apartment';
+const size = 'large';
 
 // Only change the syntax below (not the values or key names)
 
 const expenses = {
-    food: 51.7501,
-    transport:  10.2,
-}
-  
+  food: 51.7501,
+  transport: 10.2,
+};
+
 const tax = {
-    734: '3%',
-    234: '20%',
-    913: '12%',
-    415: '38%',
-    502: '42%',
-}
+  734: '3%',
+  234: '20%',
+  913: '12%',
+  415: '38%',
+  502: '42%',
+};
 
 const rent = {
-    none: 0,
-    'small-room': 200,
-    'large-room': 300,
-    'small-apartment': 400,
-    'large-apartment': 800,
-    'small-house': 1200,
-    'large-house': 2400,
-}
+  none: 0,
+  'small-room': 200,
+  'large-room': 300,
+  'small-apartment': 400,
+  'large-apartment': 800,
+  'small-house': 1200,
+  'large-house': 2400,
+};
 
 // You can change below however you want
 
-const taxAsDecimal = ('tax.913 / 100')
-const startingAfterTax = 4000 * 1 - taxAsDecimal
-const type = lodging + size
-const balance = expenses(10.2) - expenses(51.7501) - expenses(800) 
-console.log(balance = 2658.05)
+const taxAsDecimal = parseFloat(tax[913]) / 100;
+const startingAfterTax = salary * (1 - taxAsDecimal);
+const type = lodging + size;
+const rentValue = rent[type];
+const balance = salary - transport - food - rentValue;
+
+console.log(balance.toFixed(2658.05));
